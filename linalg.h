5c7e0e29220b3cb91f0ad7304455b959f06c989c
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-11-14 20:07:39 -0500

    Add examples for vector operations and functions
    NOTES:
    
    - The generic selection functions will require more reflexion before
      being implemented.

--- linalg.h
+++ linalg.h
@@ -1,15 +1,63 @@
 #ifndef LINALG_H
 #define LINALG_H
 
-#include <math.h>
+#include <assert.h>
+#include <stdarg.h>
+// #include <math.h>
+
+#if !defined(__STDDEF_H)
+typedef unsigned long size_t;
+#endif // __STDDEF_H
 
 #ifndef LINALG_DEF
 #define LINALG_DEF static inline
 #endif // LINALG_DEF
 
 typedef union {
-    struct { float x, y; };
+    struct {
+        float x, y;
+    };
     float e[2];
 } vec2f_t;
+
+LINALG_DEF vec2f_t vec2f_splat(float x) {
+    vec2f_t v = {{x, x}};
+    return v;
+}
+
+LINALG_DEF vec2f_t vec2f_add(vec2f_t a, vec2f_t b) {
+    a.x += b.x;
+    a.y += b.y;
+    return a;
+}
+
+LINALG_DEF vec2f_t vec2f_sum(size_t n, ...) {
+    va_list args;
+    va_start(args, n);
+    vec2f_t sum = {0};
+    for (size_t i = 0; i < n; ++i) {
+        vec2f_t v = va_arg(args, vec2f_t);
+        sum = vec2f_add(sum, v);
+    }
+    return sum;
+}
+
+#define lerp(X) _Generic((X), float: lerpf, double: lerpd)(X)
+
+LINALG_DEF float lerpf(float a, float b, float t) { return a + (b - a) * t; }
+
+LINALG_DEF float minf(float a, float b) { return a < b ? a : b; }
+
+LINALG_DEF float maxf(float a, float b) { return a > b ? a : b; }
+
+LINALG_DEF float clampf(float x, float min, float max) {
+    assert(min <= max && "Invalid range.");
+    if (x < min) {
+        x = min;
+    } else if (x > max) {
+        x = max;
+    }
+    return x;
+}
 
 #endif // LINALG_H
