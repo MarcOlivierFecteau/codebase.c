MarcOlivierFecteau/codebase.c

commit 0a5b809c1e28741ecab1d5ce86c4fca2ea8bc063

Implement rotation matrices constructors and functions
This commit also implements transform matrices constructors.

NOTES:

- It could be cool to have an "in place" variant of the rotation
  function.

linalg.h
@@ -13,6 +13,12 @@ typedef unsigned long size_t;
 #define LINALG_DEF static inline
 #endif // LINALG_DEF
 
+typedef enum {
+    AXIS_X = 0,
+    AXIS_Y,
+    AXIS_Z,
+} rotate_axis_s;
+
 typedef union {
     struct {
         float x, y;
@@ -275,6 +281,15 @@ LINALG_DEF mat2f_t mat2f_I(void) {
     return M;
 }
 
+LINALG_DEF mat2f_t mat2f_R(float angle) {
+    mat2f_t R;
+    R._11 = cosf(angle);
+    R._12 = -sinf(angle);
+    R._21 = sinf(angle);
+    R._22 = cosf(angle);
+    return R;
+}
+
 vec2d_t vec2d(double x, double y) {
     vec2d_t v = {{x, y}};
     return v;
@@ -295,6 +310,15 @@ LINALG_DEF mat2d_t mat2d_I(void) {
     M._11 = 1;
     M._22 = 1;
     return M;
+}
+
+LINALG_DEF mat2d_t mat2d_R(double angle) {
+    mat2d_t R;
+    R._11 = cos(angle);
+    R._12 = -sin(angle);
+    R._21 = sin(angle);
+    R._22 = cos(angle);
+    return R;
 }
 
 vec2i_t vec2i(int x, int y) {
@@ -364,6 +388,17 @@ LINALG_DEF mat3f_t mat3f_I(void) {
     return M;
 }
 
+LINALG_DEF mat3f_t mat3f_R(rotate_axis_s axis, float angle) {
+    mat3f_t R = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    R.M[i][i] = cosf(angle);
+    R.M[i][j] = -sinf(angle);
+    R.M[j][i] = sinf(angle);
+    R.M[j][j] = cosf(angle);
+    return R;
+}
+
 vec3d_t vec3d(double x, double y, double z) {
     vec3d_t v = {{x, y, z}};
     return v;
@@ -385,6 +420,17 @@ LINALG_DEF mat3d_t mat3d_I(void) {
     M._22 = 1;
     M._33 = 1;
     return M;
+}
+
+LINALG_DEF mat3d_t mat3d_R(rotate_axis_s axis, double angle) {
+    mat3d_t R = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    R.M[i][i] = cos(angle);
+    R.M[i][j] = -sin(angle);
+    R.M[j][i] = sin(angle);
+    R.M[j][j] = cos(angle);
+    return R;
 }
 
 vec3i_t vec3i(int x, int y, int z) {
@@ -457,6 +503,29 @@ LINALG_DEF mat4f_t mat4f_I(void) {
     return M;
 }
 
+LINALG_DEF mat4f_t mat4f_R(rotate_axis_s axis, float angle) {
+    mat4f_t R = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    R.M[i][i] = cosf(angle);
+    R.M[i][j] = -sinf(angle);
+    R.M[j][i] = sinf(angle);
+    R.M[j][j] = cosf(angle);
+    return R;
+}
+
+LINALG_DEF mat4f_t mat4f_T(rotate_axis_s axis, float angle) {
+    mat4f_t T = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    T.M[i][i] = cosf(angle);
+    T.M[i][j] = -sinf(angle);
+    T.M[j][i] = sinf(angle);
+    T.M[j][j] = cosf(angle);
+    T.M[2][2] = 1;
+    return T;
+}
+
 vec4d_t vec4d(double x, double y, double z, double w) {
     vec4d_t v = {{x, y, z, w}};
     return v;
@@ -479,6 +548,29 @@ LINALG_DEF mat4d_t mat4d_I(void) {
     M._33 = 1;
     M._44 = 1;
     return M;
+}
+
+LINALG_DEF mat4d_t mat4d_R(rotate_axis_s axis, double angle) {
+    mat4d_t R = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    R.M[i][i] = cos(angle);
+    R.M[i][j] = -sin(angle);
+    R.M[j][i] = sin(angle);
+    R.M[j][j] = cos(angle);
+    return R;
+}
+
+LINALG_DEF mat4d_t mat4d_T(rotate_axis_s axis, double angle) {
+    mat4d_t T = {0};
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    T.M[i][i] = cos(angle);
+    T.M[i][j] = -sin(angle);
+    T.M[j][i] = sin(angle);
+    T.M[j][j] = cos(angle);
+    T.M[2][2] = 1;
+    return T;
 }
 
 vec4i_t vec4i(int x, int y, int z, int w) {
@@ -669,6 +761,14 @@ LINALG_DEF vec2f_t mat2f_mul_vec(mat2f_t M, vec2f_t v) {
     return result;
 }
 
+LINALG_DEF mat2f_t mat2f_rotate(mat2f_t A, float angle) {
+    A._11 *= cosf(angle);
+    A._12 *= -sinf(angle);
+    A._21 *= sinf(angle);
+    A._22 *= cosf(angle);
+    return A;
+}
+
 LINALG_DEF vec2d_t vec2d_add(vec2d_t a, vec2d_t b) {
     a.x += b.x;
     a.y += b.y;
@@ -807,6 +907,14 @@ LINALG_DEF vec2d_t mat2d_mul_vec(mat2d_t M, vec2d_t v) {
     result.x = vec2d_dot(M.v[0], v);
     result.y = vec2d_dot(M.v[1], v);
     return result;
+}
+
+LINALG_DEF mat2d_t mat2d_rotate(mat2d_t A, double angle) {
+    A._11 *= cos(angle);
+    A._12 *= -sin(angle);
+    A._21 *= sin(angle);
+    A._22 *= cos(angle);
+    return A;
 }
 
 LINALG_DEF vec2i_t vec2i_add(vec2i_t a, vec2i_t b) {
@@ -1157,6 +1265,16 @@ LINALG_DEF vec3f_t mat3f_mul_vec(mat3f_t M, vec3f_t v) {
     return result;
 }
 
+LINALG_DEF mat3f_t mat3f_rotate(mat3f_t A, rotate_axis_s axis, float angle) {
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    A.M[i][i] *= cosf(angle);
+    A.M[i][j] *= -sinf(angle);
+    A.M[j][i] *= sinf(angle);
+    A.M[j][j] *= cosf(angle);
+    return A;
+}
+
 LINALG_DEF vec3d_t vec3d_add(vec3d_t a, vec3d_t b) {
     a.x += b.x;
     a.y += b.y;
@@ -1319,6 +1437,16 @@ LINALG_DEF vec3d_t mat3d_mul_vec(mat3d_t M, vec3d_t v) {
     result.y = vec3d_dot(M.v[1], v);
     result.z = vec3d_dot(M.v[2], v);
     return result;
+}
+
+LINALG_DEF mat3d_t mat3d_rotate(mat3d_t A, rotate_axis_s axis, double angle) {
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    A.M[i][i] *= cos(angle);
+    A.M[i][j] *= -sin(angle);
+    A.M[j][i] *= sin(angle);
+    A.M[j][j] *= cos(angle);
+    return A;
 }
 
 LINALG_DEF vec3i_t vec3i_add(vec3i_t a, vec3i_t b) {
@@ -1715,6 +1843,16 @@ LINALG_DEF vec4f_t mat4f_mul_vec(mat4f_t M, vec4f_t v) {
     return result;
 }
 
+LINALG_DEF mat4f_t mat4f_rotate(mat4f_t A, rotate_axis_s axis, float angle) {
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    A.M[i][i] *= cosf(angle);
+    A.M[i][j] *= -sinf(angle);
+    A.M[j][i] *= sinf(angle);
+    A.M[j][j] *= cosf(angle);
+    return A;
+}
+
 LINALG_DEF vec4d_t vec4d_add(vec4d_t a, vec4d_t b) {
     a.x += b.x;
     a.y += b.y;
@@ -1885,6 +2023,16 @@ LINALG_DEF vec4d_t mat4d_mul_vec(mat4d_t M, vec4d_t v) {
     result.z = vec4d_dot(M.v[2], v);
     result.w = vec4d_dot(M.v[3], v);
     return result;
+}
+
+LINALG_DEF mat4d_t mat4d_rotate(mat4d_t A, rotate_axis_s axis, double angle) {
+    size_t i = (axis + 1) % 3;
+    size_t j = (axis + 2) % 3;
+    A.M[i][i] *= cos(angle);
+    A.M[i][j] *= -sin(angle);
+    A.M[j][i] *= sin(angle);
+    A.M[j][j] *= cos(angle);
+    return A;
 }
 
 LINALG_DEF vec4i_t vec4i_add(vec4i_t a, vec4i_t b) {

src/linalg_generator.c
@@ -240,6 +240,12 @@ void generate_head(FILE *restrict stream) {
     fprintf(stream, "#define LINALG_DEF static inline\n");
     fprintf(stream, "#endif // LINALG_DEF\n");
     EMPTY_LINE(stream);
+    fprintf(stream, "typedef enum {\n");
+    fprintf(stream, INDENT "AXIS_X = 0,\n");
+    fprintf(stream, INDENT "AXIS_Y,\n");
+    fprintf(stream, INDENT "AXIS_Z,\n");
+    fprintf(stream, "} rotate_axis_s;\n");
+    EMPTY_LINE(stream);
 }
 
 void generate_vec_definition(FILE *restrict stream, size_t dim, type_s type) {
@@ -715,6 +721,110 @@ void generate_mat_mul_by_vec(FILE *restrict stream, size_t dim, type_s type) {
     EMPTY_LINE(stream);
 }
 
+void generate_mat_rotation_constructor(FILE *restrict stream, size_t dim,
+                                       type_s type) {
+    if (dim > 4) {
+        return; // I don't see any use case for rotating high dimension
+                // matrices.
+    }
+    if (!(type == FLOAT_T || type == DOUBLE_T)) {
+        return; // trigonometric functions do not support integer values.
+    }
+    const char *mat_prefix = mat_prefix_name(dim, type);
+    const char *type_keyword = type_definitions[type].keyword;
+    const char *type_suffix = type == FLOAT_T ? "f" : "";
+    if (dim == 2) {
+        fprintf(stream, "LINALG_DEF %s_t %s_R(%s angle) {\n", mat_prefix,
+                mat_prefix, type_keyword);
+        fprintf(stream, INDENT "%s_t R;\n", mat_prefix);
+        fprintf(stream, INDENT "R._11 = cos%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R._12 = -sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R._21 = sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R._22 = cos%s(angle);\n", type_suffix);
+    } else {
+        // NOTE: I dont't see any use case for w-axis rotation of a 4x4
+        // matrix. Most of the time, the matrix the rotation is performed
+        // on is a transform matrix, so a w-axis rotation does not make sense.
+        fprintf(stream,
+                "LINALG_DEF %s_t %s_R(rotate_axis_s axis, %s angle) {\n",
+                mat_prefix, mat_prefix, type_keyword);
+        fprintf(stream, INDENT "%s_t R = {0};\n", mat_prefix);
+        fprintf(stream, INDENT "size_t i = (axis + 1) %% 3;\n");
+        fprintf(stream, INDENT "size_t j = (axis + 2) %% 3;\n");
+        fprintf(stream, INDENT "R.M[i][i] = cos%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R.M[i][j] = -sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R.M[j][i] = sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "R.M[j][j] = cos%s(angle);\n", type_suffix);
+    }
+    fprintf(stream, INDENT "return R;\n");
+    fprintf(stream, "}\n");
+    EMPTY_LINE(stream);
+}
+
+void generate_mat_rotate(FILE *restrict stream, size_t dim, type_s type) {
+    if (dim > 4) {
+        return; // I don't see any use case for rotating high dimension
+                // matrices.
+    }
+    if (!(type == FLOAT_T || type == DOUBLE_T)) {
+        return; // trigonometric functions do not support integer values.
+    }
+    const char *mat_prefix = mat_prefix_name(dim, type);
+    const char *type_keyword = type_definitions[type].keyword;
+    const char *type_suffix = type == FLOAT_T ? "f" : "";
+    if (dim == 2) {
+        fprintf(stream, "LINALG_DEF %s_t %s_rotate(%s_t A, %s angle) {\n",
+                mat_prefix, mat_prefix, mat_prefix, type_keyword);
+        fprintf(stream, INDENT "A._11 *= cos%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A._12 *= -sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A._21 *= sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A._22 *= cos%s(angle);\n", type_suffix);
+    } else {
+        // NOTE: I dont't see any use case for w-axis rotation of a 4x4
+        // matrix. Most of the time, the matrix the rotation is performed
+        // on is a transform matrix, so a w-axis rotation does not make sense.
+        fprintf(stream,
+                "LINALG_DEF %s_t %s_rotate(%s_t A, rotate_axis_s axis, %s "
+                "angle) {\n",
+                mat_prefix, mat_prefix, mat_prefix, type_keyword);
+        fprintf(stream, INDENT "size_t i = (axis + 1) %% 3;\n");
+        fprintf(stream, INDENT "size_t j = (axis + 2) %% 3;\n");
+        fprintf(stream, INDENT "A.M[i][i] *= cos%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A.M[i][j] *= -sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A.M[j][i] *= sin%s(angle);\n", type_suffix);
+        fprintf(stream, INDENT "A.M[j][j] *= cos%s(angle);\n", type_suffix);
+    }
+    fprintf(stream, INDENT "return A;\n");
+    fprintf(stream, "}\n");
+    EMPTY_LINE(stream);
+}
+
+void generate_mat_transform_constructor(FILE *restrict stream, size_t dim,
+                                        type_s type) {
+    if (dim != 4) {
+        return;
+    }
+    if (!(type == FLOAT_T || type == DOUBLE_T)) {
+        return; // trigonometric functions do not support integer values.
+    }
+    const char *mat_prefix = mat_prefix_name(dim, type);
+    const char *type_keyword = type_definitions[type].keyword;
+    const char *type_suffix = type == FLOAT_T ? "f" : "";
+    fprintf(stream, "LINALG_DEF %s_t %s_T(rotate_axis_s axis, %s angle) {\n",
+            mat_prefix, mat_prefix, type_keyword);
+    fprintf(stream, INDENT "%s_t T = {0};\n", mat_prefix);
+    fprintf(stream, INDENT "size_t i = (axis + 1) %% 3;\n");
+    fprintf(stream, INDENT "size_t j = (axis + 2) %% 3;\n");
+    fprintf(stream, INDENT "T.M[i][i] = cos%s(angle);\n", type_suffix);
+    fprintf(stream, INDENT "T.M[i][j] = -sin%s(angle);\n", type_suffix);
+    fprintf(stream, INDENT "T.M[j][i] = sin%s(angle);\n", type_suffix);
+    fprintf(stream, INDENT "T.M[j][j] = cos%s(angle);\n", type_suffix);
+    fprintf(stream, INDENT "T.M[2][2] = 1;\n");
+    fprintf(stream, INDENT "return T;\n");
+    fprintf(stream, "}\n");
+    EMPTY_LINE(stream);
+}
+
 int main() {
     generate_head(stdout);
     for (size_t dim = VEC_MIN_SIZE; dim <= VEC_MAX_SIZE; ++dim) {
@@ -726,7 +836,8 @@ int main() {
 
     // NOTES:
     // - Constructors with math syntax only support up to 4 components;
-    // - For matrices, I think higher dimension constructors add an unnecessary
+    // - For matrices, I think higher dimension constructors add an
+    // unnecessary
     //   amount of bloat in the library.
     size_t max_supported_constructor_dim = VEC_MAX_SIZE < 4 ? VEC_MAX_SIZE : 4;
     for (size_t dim = VEC_MIN_SIZE; dim <= max_supported_constructor_dim;
@@ -735,9 +846,12 @@ int main() {
             generate_vec_constructor(stdout, dim, type);
             generate_vec_scalar_constructor(stdout, dim, type);
             // NOTE: there are no matrix constructor generated, because the
-            // number of possible ways to specify the parameters is too high.
+            // number of possible ways to specify the parameters is too
+            // high.
             generate_mat_zero_constructor(stdout, dim, type);
             generate_mat_identity_constructor(stdout, dim, type);
+            generate_mat_rotation_constructor(stdout, dim, type);
+            generate_mat_transform_constructor(stdout, dim, type);
         }
     }
 
@@ -761,6 +875,7 @@ int main() {
             generate_vec_angle_between(stdout, dim, type);
             generate_mat_mul(stdout, dim, type);
             generate_mat_mul_by_vec(stdout, dim, type);
+            generate_mat_rotate(stdout, dim, type);
         }
     }
 
@@ -774,7 +889,7 @@ int main() {
 }
 
 // Ideas for additional features:
-// - Implement rotation function (+ rotation matrices constructors);
+// - Implement transform function;
 // - Implement variadic matrix multiplication;
 // - Add support for non-square matrices (definitions, zero, mul, mul_vec);
 // - Implement integer lerping;
@@ -782,6 +897,7 @@ int main() {
 // - Implement for size casting (downsizing first, then upsizing);
 // - Add support for rudimentary (hard-coded) swizzling (?);
 // - Add support for swizzling using parsing macro (?);
-// - Add support for type generic operations and functions (C23< first, maybe
-// C99);
-// - Print statistics (loc generated, number of functions for each type, etc.).
+// - Add support for type generic operations and functions (C23< first,
+// maybe C99);
+// - Print statistics (loc generated, number of functions for each type,
+// etc.).

commit 1f7b31831f73fb290a357992ecf487e97b308be6

Fix indexing error for transform ctor introduced in 0a5b809

linalg.h
@@ -522,7 +522,7 @@ LINALG_DEF mat4f_t mat4f_T(rotate_axis_s axis, float angle) {
     T.M[i][j] = -sinf(angle);
     T.M[j][i] = sinf(angle);
     T.M[j][j] = cosf(angle);
-    T.M[2][2] = 1;
+    T.M[3][3] = 1;
     return T;
 }
 
@@ -569,7 +569,7 @@ LINALG_DEF mat4d_t mat4d_T(rotate_axis_s axis, double angle) {
     T.M[i][j] = -sin(angle);
     T.M[j][i] = sin(angle);
     T.M[j][j] = cos(angle);
-    T.M[2][2] = 1;
+    T.M[3][3] = 1;
     return T;
 }
 

src/linalg_generator.c
@@ -838,7 +838,7 @@ void generate_mat_transform_constructor(FILE *restrict stream, size_t dim,
     fprintf(stream, INDENT "T.M[i][j] = -sin%s(angle);\n", type_suffix);
     fprintf(stream, INDENT "T.M[j][i] = sin%s(angle);\n", type_suffix);
     fprintf(stream, INDENT "T.M[j][j] = cos%s(angle);\n", type_suffix);
-    fprintf(stream, INDENT "T.M[2][2] = 1;\n");
+    fprintf(stream, INDENT "T.M[3][3] = 1;\n");
     fprintf(stream, INDENT "return T;\n");
     fprintf(stream, "}\n");
     EMPTY_LINE(stream);